920d60c6131bcf224b88b29ce6700de9780b27a6
Nsyamln <maulinainasnasyaa@gmail.com>
2023-08-29 20:29:52 +0700

    tr

--- frontend/src/pages/Catalog.jsx
+++ frontend/src/pages/Catalog.jsx
@@ -1,14 +1,10 @@
-import Pagination from "@mui/material/Pagination";
 import { useEffect, useState } from "react";
-import { AiFillEye } from "react-icons/ai";
 import { Navigate, useNavigate, useOutletContext } from "react-router-dom";
 import { api } from "../utils.js";
-import { MdAdd, MdClose, MdRemove, MdShoppingCart } from "react-icons/md";
+import { MdAdd, MdClose, MdRemove } from "react-icons/md";
 import { FiShoppingCart } from "react-icons/fi";
-import { BsEmojiSmileUpsideDown } from "react-icons/bs";
 
 export default function Catalog() {
-  // const navigate = useNavigate();
   const [products, setProducts] = useState([]);
   const [keyword, setKeyword] = useState("");
   const [category, setCategory] = useState("Semua");
@@ -23,18 +19,17 @@ export default function Catalog() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setProducts(data.products);
+        setProducts(data);
       })
       .catch((error) => {
         console.error("Error fetching products:", error);
       });
   }, []);
-  console.log(products);
+
   if (user) {
     return (
-      // <h1>sasasa</h1>
-      <div className="">
-        <header className="w-full bg-col3 h-16 flex flex-row rounded-lg gap-5 items-center m-4">
+      <div className="flex flex-col m-4">
+        <header className=" bg-col3 h-16 flex justify-evenly  rounded-lg  items-center m-4">
           <label className="m-5 ">
             Cari :
             <input
@@ -61,83 +56,47 @@ export default function Catalog() {
             {cart.reduce((a, p) => a + p.count, 0)}
           </button>
         </header>
-        <main>
-          {console.log(products)}
-          {products?.map((product, i) => (
-            <div key={i}>
-              <img src="product.image" alt="" />
-              <div>
-                <h3>{product.name}</h3>
-                <h3>{product.price}</h3>
-              </div>
-              <button
-                onClick={() => {
-                  if (cart.find((p) => p.id === product.id)) {
-                    setCart(
-                      cart.map((p) =>
-                        p.id === product.id
-                          ? {
-                              ...p,
-                              count: p.count + 1,
-                            }
-                          : p
-                      )
-                    );
-                  } else {
-                    setCart([...cart, { ...product, count: 1 }]);
-                  }
-                }}
-              ></button>
-              onEdit={() => setEditedProduct(product)}
-              onDelete=
-              {() => {
-                if (
-                  confirm(
-                    `Apakah Anda yakin ingin menghapus produk ini (${product.name})?`
-                  )
-                ) {
-                  setProducts(products.filter((p) => p.id !== product.id));
-                }
-              }}
-            </div>
-          ))}
-        </main>
-        {/* <footer>
-          <label>
-            Produk per halaman:
-            <input
-              type="number"
-              value={productsPerPage}
-              onChange={(e) => setProductsPerPage(parseInt(e.target.value))}
-            />
-          </label>
-          <button onClick={() => setPage(page - 1)} disabled={page === 1}>
-            Sebelumnya
-          </button>
-          {filteredSortedProducts
-            .filter((_product, i) => i % productsPerPage === 0)
-            .map((_product, i) => (
-              <button
-                key={i}
-                className="page-number"
-                onClick={() => setPage(i + 1)}
-                disabled={i + 1 === page}
+        <div className="flex flex-wrap gap-5">
+          {products && products.length === 0 ? (
+            <p className="text-xl">Memuat Produk...</p>
+          ) : (
+            products.map((product) => (
+              <div
+                key={product.id}
+                className="bg-col2 w-fit flex  h-80 rounded-lg flex-col "
               >
-                {i + 1}
-              </button>
-            ))}
-          <button
-            onClick={() => setPage(page + 1)}
-            disabled={
-              page === Math.ceil(filteredSortedProducts.length / productsPerPage)
-            }
-          >
-            Berikutnya
-          </button>
-        </footer> */}
+                <img src={product.image} alt="" className="m-2 w-60" />
+                <div className="flex flex-col items-center justify-center">
+                  <h3 className="text-lg">{product.name_product}</h3>
+                  <h3 className="text-md">Rp {product.price}</h3>
+                </div>
+                <button
+                  onClick={() => {
+                    if (cart.find((p) => p.id === product.id)) {
+                      setCart(
+                        cart.map((p) =>
+                          p.id === product.id
+                            ? {
+                                ...p,
+                                count: p.count + 1,
+                              }
+                            : p
+                        )
+                      );
+                    } else {
+                      setCart([...cart, { ...product, count: 1 }]);
+                    }
+                  }}
+                >
+                  <FiShoppingCart />
+                </button>
+              </div>
+            ))
+          )}
+        </div>
         {isCartOpen && (
-          <div className="card dialog">
-            <button variant="tonal" onClick={() => setIsCartOpen(false)}>
+          <div className="bg-col3">
+            <button onClick={() => setIsCartOpen(false)}>
               <MdClose />
             </button>
             <h1>Keranjang</h1>
@@ -292,10 +251,6 @@ export default function Catalog() {
             </div>
           </form>
         )}
-
-        {/* <div className="flex justify-center my-4">
-        <Pagination count={10} size="large" shape="rounded" />
-      </div> */}
       </div>
     );
   } else {
